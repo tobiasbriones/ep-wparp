3fdb65f526e2c3c51a2b78aa672a7eaff3a27813
Tobias Briones <tobiasbrionesdev@gmail.com>
2020-01-09 19:36:27 -0600

    License header updated to MIT

--- _wparp/auto_reload.js
+++ _wparp/auto_reload.js
@@ -1,150 +1,132 @@
 /*
- * This file is part of example.programming.tool.php.wparp = WPARP.
+ * Copyright (c) 2019-2020 Tobias Briones.
  *
- * WPARP is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * WPARP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with WPARP.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-/*
- * Copyright (c) 2019 Tobias Briones
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 /**
  * It manages the polling algorithm.
  */
 function WPARP() {
-    
-    var active = false;
-    var pollingRequestDone = false;
-    var pollingTimeMS = 500;
-    var pollingFiles = [];
-    
-    /**
-     * Returns true iff the WPARP is running.
-     */
-    this.isRunning = () => {
-        return active;
+  var active = false;
+  var pollingRequestDone = false;
+  var pollingTimeMS = 500;
+  var pollingFiles = [];
+
+  /**
+   * Returns true iff the WPARP is running.
+   */
+  this.isRunning = () => {
+    return active;
+  };
+
+  /**
+   * Sets/Gets the polling time in ms.
+   * @param timeMS polling time in ms
+   */
+  this.pollingTime = timeMS => {
+    if (timeMS === undefined) return pollingTimeMS;
+    if (active) {
+      isRunningErrorMsg();
+      return;
     }
-    
-    /**
-     * Sets/Gets the polling time in ms.
-     * @param timeMS polling time in ms
-     */
-    this.pollingTime = (timeMS) => {
-        if(timeMS === undefined) return pollingTimeMS;
-        if(active) {
-            isRunningErrorMsg();
-            return;
-        }
-        pollingTimeMS = timeMS;
+    pollingTimeMS = timeMS;
+  };
+
+  /**
+   * Sets/Gets the array of targeted files to keep track.
+   * @param files array of targeted files
+   */
+  this.pollingFiles = files => {
+    if (files === undefined) return pollingFiles;
+    if (active) {
+      isRunningErrorMsg();
+      return;
     }
-    
-    /**
-     * Sets/Gets the array of targeted files to keep track.
-     * @param files array of targeted files
-     */
-    this.pollingFiles = (files) => {
-        if(files === undefined) return pollingFiles;
-        if(active) {
-            isRunningErrorMsg();
-            return;
-        }
-        pollingFiles = files;
+    pollingFiles = files;
+  };
+
+  /**
+   * Starts the polling service.
+   */
+  this.start = () => {
+    if (active) {
+      isRunningErrorMsg();
+      return;
     }
-    
-    /**
-     * Starts the polling service.
-     */
-    this.start = () => {
-        if(active) {
-            isRunningErrorMsg();
-            return;
-        }
-        active = true;
-        const request = new XMLHttpRequest();
-        const url = "_wparp/run.php";
-        const params = JSON.stringify(pollingFiles);
-        
-        request.addEventListener("load", () => {
-            const response = JSON.parse(request.responseText);
-            
-            if(response.status != "OK") {
-                console.error(response.status);
-                return;
-            }
-            runService();
-            console.log("WPARP Files registered: " + pollingFiles);
-            console.log("WPARP running...");
-            pollingRequestDone = true;
-        });
-        request.open("POST", url, true);
-        request.setRequestHeader("Content-Type", "application/json");
-        request.send(params);
-    }
-    
-    /**
-     * Ends the polling service.
-     * @param callback callback
-     */
-    this.end = (callback) => {
-        const request = new XMLHttpRequest();
-        
-        request.addEventListener("load", () => {
-            console.log("WPARP ended");
-            active = false;
-            
-            if(callback !== undefined) {
-                callback();
-            }
-        });
-        request.open("POST", "_wparp/exit.php", true);
-        request.send();
-    }
-    
-    function sendRequest() {
-        pollingRequestDone = false;
-        const request = new XMLHttpRequest();
-        const url = "_wparp/check.php";
-        
-        request.addEventListener("load", () => {
-            const response = JSON.parse(request.responseText);
-            
-            if(response.data.hasChanges) {
-                location.reload();
-            }
-            pollingRequestDone = true;
-        });
-        request.open("POST", url, true);
-        request.setRequestHeader("Content-Type", "application/json");
-        request.send();
-    }
-    
-    function runService() {
-        const i = setInterval(() => {
-            if(active && pollingRequestDone) {
-                sendRequest();
-            }
-            else if(!active) {
-                clearInterval(i);
-            }
-            
-        }, pollingTimeMS);
-    }
-    
-    function isRunningErrorMsg() {
-        console.error("WPARP is running, end it first.");
-    }
-    
+    active = true;
+    const request = new XMLHttpRequest();
+    const url = '_wparp/run.php';
+    const params = JSON.stringify(pollingFiles);
+
+    request.addEventListener('load', () => {
+      const response = JSON.parse(request.responseText);
+
+      if (response.status != 'OK') {
+        console.error(response.status);
+        return;
+      }
+      runService();
+      console.log('WPARP Files registered: ' + pollingFiles);
+      console.log('WPARP running...');
+      pollingRequestDone = true;
+    });
+    request.open('POST', url, true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(params);
+  };
+
+  /**
+   * Ends the polling service.
+   * @param callback callback
+   */
+  this.end = callback => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('load', () => {
+      console.log('WPARP ended');
+      active = false;
+
+      if (callback !== undefined) {
+        callback();
+      }
+    });
+    request.open('POST', '_wparp/exit.php', true);
+    request.send();
+  };
+
+  function sendRequest() {
+    pollingRequestDone = false;
+    const request = new XMLHttpRequest();
+    const url = '_wparp/check.php';
+
+    request.addEventListener('load', () => {
+      const response = JSON.parse(request.responseText);
+
+      if (response.data.hasChanges) {
+        location.reload();
+      }
+      pollingRequestDone = true;
+    });
+    request.open('POST', url, true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send();
+  }
+
+  function runService() {
+    const i = setInterval(() => {
+      if (active && pollingRequestDone) {
+        sendRequest();
+      } else if (!active) {
+        clearInterval(i);
+      }
+    }, pollingTimeMS);
+  }
+
+  function isRunningErrorMsg() {
+    console.error('WPARP is running, end it first.');
+  }
 }
 
 // Fields
@@ -152,8 +134,8 @@ const wparp = new WPARP();
 
 // DOM
 window.onbeforeunload = () => {
-    wparp.end();
-}
+  wparp.end();
+};
 
 // Set up WPARP
 //wparp.pollingTime(1000);
